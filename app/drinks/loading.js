4cd9e300c96325fda4462c0806f8c235eee53055
tedirland <irlandth@gmail.com>
2024-06-06 15:58:49 -0500

    implement loading component for drinks page

---
+++ app/drinks/loading.js
@@ -0,0 +1,5 @@
+const loading = () => {
+  return <span className="loading loading-bars loading-md"></span>;
+};
+
+export default loading;
--- app/drinks/page.jsx
+++ app/drinks/page.jsx
@@ -1,10 +1,18 @@
-import React from "react";
-import Link from "next/link";
+import { resolve } from "path";
+
 const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a";
 
-const DrinksPage = async () => {
+const fetchDrinks = async () => {
+  await new Promise((resolve) => {
+    setTimeout(resolve, 3000);
+  });
   const response = await fetch(url);
   const data = await response.json();
+  return data;
+};
+
+const DrinksPage = async () => {
+  const data = await fetchDrinks();
   console.log(data);
   return (
     <div>
