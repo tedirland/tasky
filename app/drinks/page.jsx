a0d9df6432f1ccb32eaa23b12a70f7b4ec62d3b8
tedirland <irlandth@gmail.com>
2024-06-05 21:01:36 -0500

    fetch data in drinks page

--- app/drinks/page.jsx
+++ app/drinks/page.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import Link from "next/link";
+const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a";
 
-const DrinksPage = () => {
+const DrinksPage = async () => {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
   return (
     <div>
       <h1 className="text-7xl">Drinks Page</h1>
--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Link from "next/link";
 
 const Home = () => {
+  console.log("homepage");
   return (
     <div>
       <h1 className="text-5xl mb-8 font-bold">Next.js Tutorial</h1>
